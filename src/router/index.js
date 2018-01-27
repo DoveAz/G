a73aa816b73d4a79bdb27767a3614c271b533455
doveaz <lijianmin1993@gmail.com>
2018-01-27 14:40:53 +0800

    z

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 const Router = require('koa-router')
 const router = new Router()
-const path = require('path')
 const fs = require('fs-extra')
 
-const knex=require('knex')({
-	dialect:'sqlite3',
-	connection:{
-		filename:'./data.db'
-	}
+const knex = require('knex')({
+  dialect: 'sqlite3',
+  connection: {
+    filename: './data.db'
+  }
+})
+
+router.get('/', async ctx => {
+  ctx.body = await fs.readFile('src/views/index.html', 'utf8')
 })
 
 
-router.get('/',async (ctx,next)=>{
-	ctx.body = await fs.readFile('src/views/index.html','utf8')
+router.post('/user', async ctx => {
+  await knex.table('users').insert(ctx.request.body).then()
+  ctx.body = {code: 1}
 })
 
-router.post('/user',async(ctx,next)=>{
-	await knex.table('users').insert(ctx.request.body).then()
-	ctx.body = {code:1}
+router.get('/user', async ctx => {
+  ctx.body = await knex.table('users').select().then()
 })
 
-router.get('/user',async(ctx,next)=>{
-	ctx.body = await knex.table('users').select().then()
+router.delete('/user/:id', async ctx => {
+  ctx.body = await knex.table('users').where('id', ctx.params.id).delete().then()
 })
 
 
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="modules/fs-extra/index.d.ts" />
+/// <reference path="modules/knex/index.d.ts" />
 /// <reference path="modules/koa-router/index.d.ts" />
 /// <reference path="modules/koa/index.d.ts" />
